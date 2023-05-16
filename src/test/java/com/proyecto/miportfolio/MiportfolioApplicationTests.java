3cd95c40eb6b8a1c7f6d43bca5b2f41497019a92
Gastón Costas <102261590+gastoncostas@users.noreply.github.com>
2023-05-15 21:18:08 -0300

    Add files via upload

---
+++ src/test/java/com/proyecto/miportfolio/MiportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.miportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
